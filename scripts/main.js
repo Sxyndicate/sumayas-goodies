2509552ee241ba939048e601765cd262f1954893
Sxyndicate <99019754+Sxyndicate@users.noreply.github.com>
2022-08-03 19:00:30 +0200

    update1

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,22 @@
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-bar').onclick=() =>{
+    navbar.classList.toggle('active');
+}
+
+
+
 let search = document.querySelector('.search');
 document.querySelector('#search').onclick=() =>{
     search.classList.toggle('active')
 }
+
+var swiper = new Swiper(". product-row", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
